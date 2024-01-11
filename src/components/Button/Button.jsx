6e23193d3252872ffd14b71738230c1dfaba71f3
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-01-11 22:08:06 +0200

    delet all components

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.css';
-
-export const Button = ({ loadMoreImages }) => {
-  return (
-    <button className={styles.LoadMoreBtn} onClick={loadMoreImages}>
-      Load more
-    </button>
-  );
-};
